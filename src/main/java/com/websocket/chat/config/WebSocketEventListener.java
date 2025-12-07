e7b9bec09f32758b376afe22efd819608f2a1439
flop-signing <mehedy.mmc272@gmail.com>
2025-12-07 08:21:20 +0600

    finish the disconnect event listner

--- src/main/java/com/websocket/chat/config/WebSocketEventListener.java
+++ src/main/java/com/websocket/chat/config/WebSocketEventListener.java
@@ -1,13 +1,35 @@
 package com.websocket.chat.config;
 
+import com.websocket.chat.controller.ChatMessage;
+import com.websocket.chat.controller.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messagingTemplate;
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String userName=accessor.getSessionAttributes().get("username").toString();
 
+        if(userName!=null){
+            log.info("User disconnected from Chat: {}", userName);
 
+            var chatMessage = ChatMessage.builder()
+                    .messageType(MessageType.LEAVE)
+                    .sender(userName)
+                    .build();
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
